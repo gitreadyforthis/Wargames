e7881b365e0919906dfb974e603f83a52589d597
Hunter <brianreiskin@mail.adelphi.edu>
2017-12-04 16:28:48 -0500

    push assignments
    Signed-off-by: Hunter <brianreiskin@mail.adelphi.edu>

--- src/WarGames/Assignments.java
+++ src/WarGames/Assignments.java
@@ -1,43 +1,61 @@
 package WarGames;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 /**
  * The class for Assignments
  * has a grade value (int) and values for students
  */
 
-public class Assignments implements Assignment {
-    Student studentResponse;
-    private int grade;
+public class Assignments {
 
-    public Assignments(int grade) {
-        this.grade = grade;
-        studentResponse = new Student();
-    }
+    private File file1 = new File("src/WarGames/files/grades.txt");
+    private File file2 = new File("src/WarGames/files/Assignments.txt");
+    private Scanner scan = null;
 
     /**
      * getGrade() returns a grade value associated
      * with the assignment object
      */
-    private int getGrade(int grade) {
-        return grade;
+    public int getGrade(String myStudent, String myProfessor, String myCourse, String myAssignment) {
+        int assGrade = 0;
+        try {
+            scan = new Scanner(file2);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        while (scan.hasNextLine()) {
+            String stuff = scan.nextLine();
+            StringTokenizer myTokenizer = new StringTokenizer(stuff, ",");
+            String student = myTokenizer.nextToken();
+            String professor = myTokenizer.nextToken();
+            String course = myTokenizer.nextToken();
+            String assignmentNo = myTokenizer.nextToken();
+            String submission = myTokenizer.nextToken();
+            String grade = myTokenizer.nextToken();
+            String visibility = myTokenizer.nextToken();
+
+            if (student.equals(myStudent) && professor.equals(myProfessor) && course.equals(myCourse) && assignmentNo.equals(myAssignment)) {
+                if (visibility.equals("true")) {
+                    if (!grade.equals("grade")) {
+                        assGrade = Integer.parseInt(grade);
+                    } else
+                        System.out.print("Assignment has not been graded yet");
+                } else
+                    System.out.print("Assignment Does not exist");
+            } else
+                System.out.print("No assignment found for parameters");
+        }
+        scan.close();
+
+        return assGrade;
     }
 
-    /**
-     * hasGrade() checks to see if the assignment has a
-     * grade associated with it or not
-     * returns false until assignment has grade added
-     */
-    private boolean hasGrade() {
-        return false;
-    }
 
-    /**
-     * boolean isPosted()
-     * checks if the assignment is posted
-     */
 
-    @Override
-    public boolean isPosted() {
-        return false;
-    }
+
 }
